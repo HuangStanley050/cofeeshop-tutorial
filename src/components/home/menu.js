8b0a1afd389b187d3d41e4431fd39bcab31f64cf
Stanley Huang <huang.stanley050@gmail.com>
2019-05-01 15:36:32 +1000

    filter function done

--- src/components/home/menu.js
+++ src/components/home/menu.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Title from "../global/title";
 import Img from "gatsby-image";
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col, Button } from "reactstrap";
 
 class Menu extends Component {
   state = {
@@ -14,8 +14,25 @@ class Menu extends Component {
       coffeeItems: this.props.items.edges,
     });
   }
+  getCategories = items => {
+    let tempCategories = new Set(items.map(item => item.node.category));
+    return ["all", ...Array.from(tempCategories)];
+  };
+  handleItems = item => {
+    //console.log(item);
+    let tempItems = [...this.state.items];
+    if (item === "all") {
+      return this.setState({ coffeeItems: tempItems });
+    } else {
+      let filterItems = this.state.items.filter(({ node }) => {
+        return node.category === item;
+      });
+      this.setState({ coffeeItems: filterItems });
+    }
+  };
 
   render() {
+    // console.log(this.getCategories(this.state.coffeeItems));
     let content = (
       <section className="menu py-5">
         <Container>
@@ -34,6 +51,20 @@ class Menu extends Component {
           <Container>
             <Title title="best of our menu" />
             <Row className="mb-5">
+              <Col xs="10" className="mx-auto text-center">
+                {this.getCategories(this.state.items).map(item => (
+                  <Button
+                    onClick={() => this.handleItems(item)}
+                    outline
+                    className="btn-yellow m-3 text-capitalize"
+                    key={item}
+                  >
+                    {item}
+                  </Button>
+                ))}
+              </Col>
+            </Row>
+            <Row>
               {this.state.coffeeItems.map(({ node }) => {
                 return (
                   <Col
@@ -48,8 +79,12 @@ class Menu extends Component {
                     </div>
                     <div className="flex-grow-1 px-3">
                       <div className="d-flex justify-content-between">
-                        <h6 className="mb-0">{node.titile}</h6>
-                        <h6 className="mb-0">${node.price}</h6>
+                        <h6 className="mb-0">
+                          <small>{node.titile}</small>
+                        </h6>
+                        <h6 className="mb-0 text-yellow">
+                          <small>${node.price}</small>
+                        </h6>
                       </div>
                       <p className="text-muted">
                         <small>{node.description.description}</small>
